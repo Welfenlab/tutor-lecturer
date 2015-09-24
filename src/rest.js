08a45e8e7895d76cca4e7162068d1b661a6fbfb8
Maximilian Klein <dasmaxl@gmail.com>
2015-09-24 14:05:19 +0200

    (feature) Basic REST Api.

--- src/rest.js
+++ src/rest.js
@@ -2,6 +2,15 @@
 module.exports = function(DB){
   return [
     { path: '/api/exercises', dataCall: DB.Exercises.get, apiMethod: "get" },
-    
+    { path: '/api/exercises/detailed/:id', dataCall: DB.Exercises.getDetailed, apiMethod: "getByParam", param: "id" },
+    { path: '/api/exercises', dataCall: DB.Manage.storeExercise, apiMethod: "putByBodyParam", param: "exercise" },
+
+    { path: '/api/tutors', dataCall: DB.Manage.listTutors, apiMethod: "get"},
+    { path: '/api/tutors', dataCall: DB.Manage.storeTutor, apiMethod: "putByBodyParam", param: "tutor"},
+
+    { path: '/api/groups', dataCall: DB.Manage.listGroups, apiMethod: "get" },
+
+    { path: '/api/users', dataCall: DB.Manage.listUsers, apiMethod: "get"},
+
   ];
 };
--- test/rest.js
+++ test/rest.js
@@ -66,16 +66,6 @@ describe("Student REST API", function(){
         done();
       });
   });
-  it("list of exercises should not contain tasks",function(done){
-    doRequest("GET","/exercises",
-      function(err, res, body){
-        (err == null).should.be.true;
-        body.forEach(function(ex){
-          ex.should.not.have.key("tasks");
-        });
-        done();
-      });
-  });
 
   it("returns a detailed list that should contain tasks", function(done){
     doRequest("GET", "/exercises/detailed/ee256059-9d92-4774-9db2-456378e04586",
@@ -89,75 +79,12 @@ describe("Student REST API", function(){
     });
   });
 
-  it("should return the number of pending solutions", function(done){
-    doRequest("GET", "/correction/pending/ee256059-9d92-4774-9db2-456378e04586",
-      function(err, res, body){
-        (err == null).should.be.true;
-        body.should.equal(2);
-        done();
-    });
-  });
-
-  it("has a method for storing the result without finalizing", function(done){
-    doSimpleRequest("PUT", "/correction/store", {id: "85ca34c0-61d6-11e5-97cb-685b35b5d746", results:[]},
-      function(err, res, body){
-        (err == null).should.be.true;
-        res.statusCode.should.equal(204);
-        done();
-    });
-  });
-
-  it("should return the status of all corrections", function(done){
-    doRequest("GET", "/correction",
-      function(err, res, body){
-        (err == null).should.be.true;
-        res.statusCode.should.equal(200);
-        body.should.have.length(2);
-        body.should.deep.include.members([
-          { exercise: 'ee256059-9d92-4774-9db2-456378e04586', solutions: 2, corrected: 0, locked: 1 },
-          { exercise: 'f31ad341-9d92-4774-9db2-456378e04586', solutions: 0, corrected: 0, locked: 0 }
-        ]);
-        done();
-    })
-  });
-
-  it("should (on demand) assign a new solution to the tutor", function(done){
-    doRequest("GET", "/correction/next/ee256059-9d92-4774-9db2-456378e04586",
-      function(err, res, body){
-        (err == null).should.be.true;
-        res.statusCode.should.equal(200);
-        body.group.should.equal("fd8c6b08-572d-11e5-9824-685b35b5d746");
-        done();
-      }
-    )
-  });
-
-  it("should list all unfinished corrections", function(done){
-    doRequest("GET", "/correction/unfinished",
+  it("should be possible to get all tutors", function(done){
+    doRequest("GET", "/tutors",
       function(err, res, body){
         (err == null).should.be.true;
         res.statusCode.should.equal(200);
         body.should.have.length(1);
-        done();
-    });
-  });
-
-  it("should finalize exercises for a tutor", function(done){
-    doSimpleRequest("POST", "/correction/finish", {id: "85ca34c0-61d6-11e5-97cb-685b35b5d746"},
-      function(err, res, body){
-        (err == null).should.be.true;
-        res.statusCode.should.equal(204);
-        done();
-    });
-  });
-
-  it("should be possible to get user information", function(done){
-    doRequest("GET", "/tutor",
-      function(err, res, body){
-        (err == null).should.be.true;
-        res.statusCode.should.equal(200);
-        body.name.should.equal("tutor");
-        body.should.not.have.key("pw");
         done();
     });
   });
