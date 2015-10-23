b3403cf5bb40dedaff4665adb444f2a4a14e7310
Maximilian Klein <mklein@welfenlab.de>
2015-10-23 13:02:08 +0200

    (feature) Add tutors via api.

--- src/rest.js
+++ src/rest.js
@@ -15,10 +15,13 @@ module.exports = function(DB){
     { path: '/api/tutors', dataCall: function(tutor){
       // first generate some salt and create a password hash ;)
       // the salt is stored in the hash
-      return bcrypt.hash(tutor.pw, 10).then(
-        _.partial(DB.Manage.storeTutor, tutor.name)
+      return bcrypt.hash(tutor.password, 10).then(
+        function(pw){
+          tutor.password = pw;
+          return DB.Manage.storeTutor(tutor)
+        }
       );
-    }, apiMethod: "putByBodyParam", param: "tutor"},
+    }, apiMethod: "postByBodyParam", param: "tutor"},
 
     { path: '/api/groups', dataCall: DB.Manage.listGroups, apiMethod: "get" },
 
